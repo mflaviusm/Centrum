f9fb8adfa703361f4aa68d112081af6f328a59ae
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-06-06 11:45:52 +0000

    create templates folder in projects app and move projects templates

--- centrum/projects/views.py
+++ centrum/projects/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 
 def project(request, pk):
-    return HttpResponse('project number' + ' ' + str(pk))
-
+    return render(request, 'projects/individual-project.html')
