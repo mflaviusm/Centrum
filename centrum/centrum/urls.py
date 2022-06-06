eb08da7e261292627081eb12b0b6156c6c32fe77
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-06-06 10:14:44 +0000

    create urls.py in projects app

--- centrum/centrum/urls.py
+++ centrum/centrum/urls.py
@@ -14,15 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def projects(request):
-    return HttpResponse('Testing path')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', projects, name="projects")
+    path('', include('projects.urls'))
 ]
-
--- centrum/projects/views.py
+++ centrum/projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def projects(request):
+    return HttpResponse('Testing path')
+
+
+def project(request, pk):
+    return HttpResponse('project number' + ' ' + str(pk))
+
