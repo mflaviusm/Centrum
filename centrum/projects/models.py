adc4e592093e33cc97ddeb26b6b2989c4119e15a
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-06-07 14:06:56 +0000

    fix models not showing in admin

--- centrum/projects/models.py
+++ centrum/projects/models.py
@@ -1,5 +1,5 @@
-import uuid
 from django.db import models
+import uuid
 
 
 class Project(models.Model):
@@ -11,6 +11,31 @@ class Project(models.Model):
     source_link = models.CharField(max_length=1000)
     vote_total = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
+    # tags = 
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'up'),
+        ('down', 'down'),
+    )
+
+    # owner=
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                            primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
