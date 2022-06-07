171ea1cf0e7dfd16d7ca2142e5d8a301573870f7
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-06-07 13:44:08 +0000

    add project model and create superuser

--- centrum/projects/models.py
+++ centrum/projects/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    # owner=
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # featured_image=
+    demo_link = models.CharField(max_length=1000,)
+    source_link = models.CharField(max_length=1000)
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
--- centrum/projects/views.py
+++ centrum/projects/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Econ',
+        'description': 'Project about e-business ideas.'
+    },
+    {
+        'id': '2',
+        'title': 'MarketMe',
+        'description': 'Website for professionals to showcast their work'
+    },
+    {
+        'id': '3',
+        'title': 'Munchy',
+        'description': 'A blog for gourmets to discuss everything food related.'
+    }
+]
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    item = 'projects'
+    number = 10
+    context = {'item': item, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/individual-project.html')
+    projectNumber = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectNumber = i
+    return render(request, 'projects/individual-project.html', {'project': projectNumber})
