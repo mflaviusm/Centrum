0e80706d0b70ea90f2df703f413d805bf0c6c420
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-06-07 18:55:37 +0000

    add skills functionality

--- centrum/users/models.py
+++ centrum/users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+# Create your models here.
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -36,3 +40,16 @@ class Profile(models.Model):
         except:
             url = ''
         return url
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
