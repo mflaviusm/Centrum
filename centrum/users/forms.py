12451f3353635a122fc4eeb6f8757be1a39dadc1
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-06-07 18:52:39 +0000

    add user profile editing functionality

--- centrum/users/forms.py
+++ centrum/users/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, Skill, Message
+from .models import Profile
 
 
 class UserCreationForm(UserCreationForm):
@@ -14,6 +14,21 @@ class UserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'email', 'username',
+                  'location', 'bio', 'short_intro', 'profile_image',
+                  'social_github', 'social_linkedin', 'social_twitter',
+                  'social_youtube', 'social_website']
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
