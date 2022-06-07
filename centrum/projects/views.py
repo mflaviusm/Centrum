ea41b012641dca51e433ffcbbe3047f5eaf277ef
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-06-07 16:00:49 +0000

    add delete form

--- centrum/projects/views.py
+++ centrum/projects/views.py
@@ -29,11 +29,24 @@ def createProject(request):
 
 
 def updateProject(request, pk):
+    context = {}
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
+    template = 'projects/project-form.html'
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context['form']= form
+    return render(request, template, context)
 
-    context = {'form': form}
-    return render(request, 'projects/project-form.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    return render(request, 'projects/delete.html', {'object': project})
